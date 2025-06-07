c7d31f3377f9b463aa99e7ae5dfd1e9b51225d81
david <89749147+davight@users.noreply.github.com>
2025-06-07 13:39:04 +0200

    should be E instead of T

--- src/util/WeightedRandomness.java
+++ src/util/WeightedRandomness.java
@@ -6,9 +6,9 @@ import java.util.Map;
 /**
  * Pomocna trieda na ulozenie T prvkov s n pravdepodobnostou vyberu.
  */
-public class WeightedRandomness<T> {
+public class WeightedRandomness<E> {
 
-    private final HashMap<T, Integer> weights = new HashMap<>();
+    private final HashMap<E, Integer> weights = new HashMap<>();
     private int sumWeights = 0;
 
     /**
@@ -19,20 +19,20 @@ public class WeightedRandomness<T> {
 
     /**
      * Prida prvok do zoznamu, s pravdepodobnostou vyberu 1.
-     * @param t prvok na pridanie
+     * @param e prvok na pridanie
      */
-    public void add(T t) {
-        this.add(t, 1);
+    public void add(E e) {
+        this.add(e, 1);
     }
 
     /**
      * Prida prvok do zoznamu.
-     * @param t prvok na pridanie
+     * @param e prvok na pridanie
      * @param weight pravdepodobnost vyberu
      */
-    public void add(T t, int weight) {
-        if (!this.weights.containsKey(t)) {
-            this.weights.put(t, weight);
+    public void add(E e, int weight) {
+        if (!this.weights.containsKey(e)) {
+            this.weights.put(e, weight);
             this.sumWeights += weight;
         }
     }
@@ -42,10 +42,10 @@ public class WeightedRandomness<T> {
      * <a href="https://stackoverflow.com/questions/6737283/weighted-randomness-in-java">odkaz na diskusiu</a>
      * @return Nahodne vybrany prvok
      */
-    public T getRandom() {
+    public E getRandom() {
         double r = Util.randomDouble() * this.sumWeights;
         int c = 0;
-        for (Map.Entry<T, Integer> e : this.weights.entrySet()) {
+        for (Map.Entry<E, Integer> e : this.weights.entrySet()) {
             c += e.getValue();
             if (c >= r) {
                 return e.getKey();
